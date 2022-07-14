d62c9cb89038f039d518e86500b9cc055548083f
Tong Lor <tonglor@nobleacademy.local>
2022-07-14 09:56:33 -0500

    fixed codes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-//const { route } = require('../htmlRoutes');
 const db = require('../../db/db.json')
 const router = require('express').Router();
 const uniqid = require('uniqid');
@@ -19,8 +18,6 @@ router.post('/api/notes', (req, res) => {
         title: req.body.title,
         text: req.body.text
     }
-    //  console.log(typeof db)
-    // //1. use fs to read the db.json
     fs.readFile('./db/db.json', (err, data) => {
         if (err) throw err;
 
@@ -35,13 +32,6 @@ router.post('/api/notes', (req, res) => {
             res.send('successfully added');
         })
     });
-    // //2. create a variable and parse the data which got returned
-
-    // //3. push the new Note to that parsed data
-
-    // //4. use fs to write new content of that parsed data to db.json file
-    // req.body.id = notes.length.toString();
-
 
 })
 
