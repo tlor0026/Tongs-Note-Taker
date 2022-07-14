de64d1bafa9df1028858b167636833d4aee98c54
Tong Lor <tonglor@nobleacademy.local>
2022-07-14 07:28:06 -0500

    new additions

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const apiRoutes = require('./routes/api');
-const htmlRoutes = require('./routes/html');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
