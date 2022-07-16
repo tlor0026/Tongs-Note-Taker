81eb8c57fdf070df9141897adc610fb753d8878c
tlor0026 <tlor0026@gmail.com>
2022-07-16 00:10:19 -0500

    hopefully final changes to names

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const api = require('./routes/api');
-const html = require('./routes/html');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(api);
-app.use(html);
+app.use(apiRoutes);
+app.use(htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
